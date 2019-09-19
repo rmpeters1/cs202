b863d104733634b0c417c15b56a7b38bc87a8c28
Rose Peters <rmpeters3@alaska.edu>
2019-09-19 07:57:47 -0800

    added using/include statements

--- lab4/lab4/streams.cpp
+++ lab4/lab4/streams.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+#include <string>
+using std::string;
+using std::getline;
+#include <sstream>
+using std::istringstream;
 
 int main() {
 
