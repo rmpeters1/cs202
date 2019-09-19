9e83c89da29c682f8c68d233e35d98a49a3fda82
Rose Peters <rmpeters3@alaska.edu>
2019-09-19 08:19:05 -0800

    removed ofstream output

--- lab4/lab4/streams.cpp
+++ lab4/lab4/streams.cpp
@@ -12,9 +12,6 @@ using std::getline;
 using std::istringstream;
 
 int main() {
-	ofstream fout("output.txt");
-	fout << "Hello";
-	fout.close();
 	ifstream fin("output.txt");
 	string s1;
 	fin >> s1;
@@ -22,8 +19,9 @@ int main() {
 	string words;
 	cout << "Please type a string: " << endl;
 	getline(cin, words);
-	cout << words;
 	int wordamnt;
 	cout << "Please type a number: " << endl;
 	cin >> wordamnt;
+	
+
 }
