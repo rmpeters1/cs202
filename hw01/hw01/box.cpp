8e8026cb12110b5b66b061e7ecb2145f43c6e609
Rose Peters <rmpeters3@alaska.edu>
2019-09-21 22:15:15 -0800

    added get/setWidth functions

--- hw01/hw01/box.cpp
+++ hw01/hw01/box.cpp
@@ -1,13 +1,22 @@
 #include "box.hpp"
 
 int Box::getHeight() const {
-	return _height;e
+	return _height;
 }
 
 void Box::setHeight(int height) {
 	_height = height;
 }
 
+int Box::getWidth() const {
+	return _width;
+}
+
+void Box::setWidth(int width) {
+	_width = width;
+}
+
 Box::Box() : _height(1), _width(1) {}
 
 Box::Box(int height, int width) : _height(height), _width(width) {}
+
