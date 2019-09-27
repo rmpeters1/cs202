cc6d4d49e3e8a23211898c993119f27d426fa198
Rose Peters <rmpeters3@alaska.edu>
2019-09-26 22:02:28 -0800

    added hw02

--- hw02/hw02/files.hpp
+++ hw02/hw02/files.hpp
@@ -1,1 +1,15 @@
-#pragma once
+/**
+* @file     files.hpp
+* @author   Rose Peters
+* @date     September 26, 2019
+* Dr. Chris Hartman
+*/
+
+#ifndef FILES_HPP_
+#define FILES_HPP_
+
+
+
+
+
+#endif //!FILES_HPP
