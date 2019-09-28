42428c1fae32c923f0db5bf40534aad5674b3e0f
Rose Peters <rmpeters3@alaska.edu>
2019-09-27 17:49:28 -0800

    added to hpp, added getIdFromFile()

---
+++ hw02/hw02/files.hpp
@@ -0,0 +1,16 @@
+#ifndef FILES_HPP_
+#define FILES_HPP_
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+using std::ostringstream;
+
+int getIdFromFile(const string & filename,
+	istringstream & istream, ostringstream & ostream);
+
+#endif //!FILES_HPP
