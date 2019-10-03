80f67f04a7169dae2d482aa8202ef6f7db42916d
Rose Peters <rmpeters3@alaska.edu>
2019-10-03 09:07:33 -0800

    made changes

--- hw02/hw02/files.cpp
+++ hw02/hw02/files.cpp
@@ -3,19 +3,32 @@
 
 int getIdFromFile(const string & filename,
 	istringstream & istream, ostringstream & ostream) {
+	string usernames;
 	int ids;
 	
+	
 	std::ifstream fin("filename");
-	cout << istream.str() << endl;
 	
+	
+	cout << istream.str();
 	while (true) {
-	
+		fin >> ids;
+		vector<int> idVec;
+		idVec.push_back(ids);
+		//for loop for names, search number in names
+		fin >> usernames;
+		for (auto numbers : idVec) {
+			if (usernames == istream.str())
+				cout << idVec[numbers];
+		 }
+		
 		if (!fin) {
 			if (fin.eof()) {
 				return -1;
 			}
 		}
 	}
+	cout << ids;
 	return 0;
 }
 
