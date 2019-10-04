b98a6d365b20422472c78bd4d98c69d8b1d27366
Rose Peters <rmpeters3@alaska.edu>
2019-10-03 21:26:26 -0800

    can read file

--- hw02/hw02/files.cpp
+++ hw02/hw02/files.cpp
@@ -3,32 +3,24 @@
 
 int getIdFromFile(const string & filename,
 	istringstream & istream, ostringstream & ostream) {
-	string usernames;
-	int ids;
 	
-	
-	std::ifstream fin("filename");
-	
-	
-	cout << istream.str();
+	string text;
+	std::ifstream fin(filename);
+	//cout << istream.str();
 	while (true) {
-		fin >> ids;
-		vector<int> idVec;
-		idVec.push_back(ids);
-		//for loop for names, search number in names
-		fin >> usernames;
-		for (auto numbers : idVec) {
-			if (usernames == istream.str())
-				cout << idVec[numbers];
-		 }
+		fin >> text;
+		
+		
+		
 		
 		if (!fin) {
+			return -1;
 			if (fin.eof()) {
-				return -1;
+				break;
 			}
 		}
 	}
-	cout << ids;
+	
 	return 0;
 }
 
