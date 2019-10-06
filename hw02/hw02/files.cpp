57fa84c7e04bb15fda47bb197cd72a4c807a47b0
Rose Peters <rmpeters3@alaska.edu>
2019-10-05 22:07:52 -0800

    size of string's out of range

--- hw02/hw02/files.cpp
+++ hw02/hw02/files.cpp
@@ -5,34 +5,47 @@ int getIdFromFile(const string & filename,
 	istringstream & istream, ostringstream & ostream) {
 	
 	std::ifstream fin(filename);
-	string names;
-	getline(istream, names);
-	vector<string> v;
-	v.push_back(names);
+	//for (auto i : istream.str()) {
+	//	cout << i;
+	//}
 	string filetext;
-
+	
 	while (true) {
-		
 		getline(fin, filetext);
-		for (auto i = 0; i < v.size(); i++) {
-			//cout << v[i];
-			if (filetext[i] == names[i]) {
-				getline(fin, filetext);
-				cout << filetext << "\n";
+
+		if (istream.str().size() > 0) {
+			for (auto i = 0; i < istream.str().size(); i++) {
+				if (istream.str()[i] == filetext[i]) {
+
+}
 			}
-			else cout << "error\n";
 		}
 		
+
+
+
+		//for (auto i = 0; i < v.size(); i++) {
+		//	//cout << v[i];
+		//	if (filetext[i] == names[i]) {
+		//		fin >> filetext;
+		//		ostream << filetext << "\n";
+		//	}
+		//	else {
+		//		ostream << "error\n";
+		//	}
+		//}
+		//
 		if (!fin) {
 			return -1;
 			if (fin.eof()) {
 				break;
 			}
 		}
+		//cout << ostream.str();
 		return 0;
 
 	}
-	
+	return 0;
 }
 
 void numberChase(const string & filename, ostringstream & ostream) {
