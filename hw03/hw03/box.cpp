9507b789f673bc0be952aa4b25c0b77d45b3f09c
Rose Peters <rmpeters3@alaska.edu>
2019-10-09 21:30:40 -0800

    added class from hw01, made enum Boxtype

---
+++ hw03/hw03/box.cpp
@@ -0,0 +1,84 @@
+/**
+* @file     box.cpp
+* @author   Rose Peters
+* @date     October 9, 2019
+* Dr. Chris Hartman
+*/
+
+#include "box.hpp"
+
+int Box::getHeight() const {
+	return _height;
+}
+
+void Box::setHeight(int height) {
+	_height = height;
+}
+
+int Box::getWidth() const {
+	return _width;
+}
+
+void Box::setWidth(int width) {
+	_width = width;
+}
+
+Box::Box() : _height(1), _width(1), _type(true) {}
+
+Box::Box(int width, int height) :
+	_height(height),
+	_width(width)
+{}
+
+Box::Box(int width, int height, bool type) :
+	_height(height),
+	_width(width),
+	_type(type)
+{}
+
+string Box::type() const {
+	if (_type == true) {
+		return "Filled";
+	}
+	else {
+		return "Hollow";
+	}
+
+}
+
+void Box::print(ostringstream& ostream) const {
+
+	for (int border = 0; border < _width; border++) {
+		if (_width != 1 && _height != 1) {
+			ostream << "x";
+		}
+	}
+	if (_width != 1 && _height != 1) ostream << endl;
+	for (int line = 0; line < _height - 2; line++) {
+		for (int col = 0; col < 1; col++) {
+			ostream << "x";
+		}
+		for (int row = 0; row < _width - 2; row++) {
+			if (_type == true) {
+				ostream << "x";
+			}
+			else if (_type) {
+				ostream << "x";
+			}
+			else {
+				ostream << " ";
+			}
+		}
+		for (int col = 0; col < 1; col++) {
+			ostream << "x";
+		}
+		ostream << endl;
+	}
+	for (int border = 0; border < _width; border++) {
+		ostream << "x";
+	}
+	ostream << endl;
+
+	//cout << ostream.str();
+
+}
--- hw03/hw03/box.hpp
+++ hw03/hw03/box.hpp
@@ -1,4 +1,42 @@
+/**
+* @file     box.cpp
+* @author   Rose Peters
+* @date     October 9, 2019
+* Dr. Chris Hartman
+*/
+
 #ifndef BOX_HPP_
 #define BOX_HPP_
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+#include <sstream>
+using std::ostringstream;
+
+class Box {
+public:
+	enum Boxtype{FILLED, HOLLOWED, CHECKERED};
+	Box();
+	Box(int width, int height);
+	Box(int width, int height, enum Boxtype);
+
+
+
+	int getHeight() const;
+	void setHeight(int height);
+	int getWidth() const;
+	void setWidth(int width);
+	string type() const;
+	void print(ostringstream& ostream) const;
+
+private:
+	int _height;
+	int _width;
+	bool _type;
+};
 
 #endif //!BOX_HPP_
