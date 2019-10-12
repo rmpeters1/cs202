12ebba6e3fa5d13f2b55965ab0a83166ac5b2417
Rose Peters <rmpeters3@alaska.edu>
2019-10-11 21:04:44 -0800

    changes to operator<<

--- hw03/hw03/box.cpp
+++ hw03/hw03/box.cpp
@@ -31,7 +31,8 @@ Box::Box() : _height(1), _width(1), _type(FILLED) {
 
 Box::Box(int width, int height) :
 	_height(height),
-	_width(width)
+	_width(width),
+	_type(FILLED)
 {
 	boxcount++;
 }
@@ -74,9 +75,9 @@ int Box::howMany() {
 ostringstream& operator<<(ostringstream& out, const Box& b) {
 
 	for (int border = 0; border < b.getWidth(); border++) {
-		if (b.type() == "CHECKERED") {
-			out << "a" << endl;
-		}
+		if (b.type()=="CHECKERED") {
+		out << "a" << endl;
+	}
 		if (b.getWidth() != 1 && b.getHeight() != 1) {
 			out << "x";
 
@@ -90,10 +91,10 @@ ostringstream& operator<<(ostringstream& out, const Box& b) {
 			out << "x";
 		}
 		for (int row = 0; row < b.getWidth() - 2; row++) {
-			if (b.type() == "FILLED") {
+			if (Box::FILLED) {
 				out << "x";
 			}
-			else if (b.type() == "HOLLOW") {
+			else if (Box::HOLLOW) {
 				out << " ";
 			}
 		}
@@ -105,44 +106,8 @@ ostringstream& operator<<(ostringstream& out, const Box& b) {
 	for (int border = 0; border < b.getWidth(); border++) {
 		out << "x";
 	}
+	
 	out << endl;
 	cout << out.str();
 	return out;
 }
-
-//void Box::print(ostringstream& ostream) const {
-
-	//for (int border = 0; border < _width; border++) {
-	//	if (_width != 1 && _height != 1) {
-	//		ostream << "x";
-	//	}
-	//}
-	//if (_width != 1 && _height != 1) ostream << endl;
-	//for (int line = 0; line < _height - 2; line++) {
-	//	for (int col = 0; col < 1; col++) {
-	//		ostream << "x";
-	//	}
-	//	for (int row = 0; row < _width - 2; row++) {
-	//		if (_type == FILLED) {
-	//			ostream << "x";
-	//		}
-	//		/*else if (_type) {
-	//			ostream << "x";
-	//		}*/
-	//		else {
-	//			ostream << " ";
-	//		}
-	//	}
-	//	for (int col = 0; col < 1; col++) {
-	//		ostream << "x";
-	//	}
-	//	ostream << endl;
-	//}
-	//for (int border = 0; border < _width; border++) {
-	//	ostream << "x";
-	//}
-	//ostream << endl;
-
-	////cout << ostream.str();
-
-//}
