e912cf3afd5a1a384742f49a0b24f9036272e9f4
Rose Peters <rmpeters3@alaska.edu>
2019-10-15 10:07:29 -0800

    Added getters/setter functions and constructor to hpp

--- hw04/hw04/money.hpp
+++ hw04/hw04/money.hpp
@@ -1,8 +1,20 @@
 #ifndef MONEY_HPP_
 #define MONEY_HPP_
 
-class Money() {
+class Money {
+public:
+	Money();
 
+	int	getDollars() const;
+	void setDollars();
+	int getCents() const;
+	void setCents();
+	int getDollarAndCents();
+	void setDollarAndCents();
+private:
+	int _dollars;
+	int _cents;
+	double _dollarAndCents;
 };
 
 #endif //!MONEY_HPP_
