b21a8b2924b4a7c7154e68ef54daa7a0d4ee124c
Rose Peters <rmpeters3@alaska.edu>
2019-11-01 08:53:43 -0800

    base class Box now has constructors and member functions

--- hw05/hw05/boxes3.hpp
+++ hw05/hw05/boxes3.hpp
@@ -1,6 +1,25 @@
 #ifndef BOXES3_HPP_
 #define BOXES3_HPP_
 class Box {
-	
+public:
+	Box(int width, int height) :
+		_height(height),
+		_width(width)
+	{}
+	int getWidth() const {
+		return _width;
+	}
+	void setWidth(int width) {
+		_width = width;
+	}
+	int getHeight() const {
+		return _height;
+	}
+	void setHeight(int height) {
+		_height = height;
+	}
+private:
+	int _height;
+	int _width;
 };
 #endif //!BOXES3_HPP_
