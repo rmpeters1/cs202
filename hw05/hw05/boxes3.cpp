7a4348229ffea0b5dbd58bd36e08c366f7b997a7
Rose Peters <rmpeters3@alaska.edu>
2019-11-05 14:40:32 -0900

    added constructors to set height and width to derived classes

--- hw05/hw05/boxes3.cpp
+++ hw05/hw05/boxes3.cpp
@@ -7,7 +7,8 @@
 
 class Filled : public Box {
 public:
-
+	Filled(int width, int height):
+		Box(width, height) {}
 	void print(ostream& os) const override {}
 	string type(string boxType) const override {}
 	unique_ptr<Box> boxFactory(char c, int w, int h) {}
@@ -15,6 +16,8 @@ public:
 
 class Hollow : public Box {
 public:
+	Hollow(int width, int height) :
+		Box(width, height) {}
 	void print(ostream& os) const override {}
 	string type(string boxType) const override {}
 	unique_ptr<Box> boxFactory(char c, int w, int h) {}
@@ -23,6 +26,8 @@ public:
 
 class Checkered : public Box {
 public:
+	Checkered(int width,int height) :
+	Box(width,height) {}
 	void print(ostream& os) const override {}
 	string type(string boxType) const override {}
 	unique_ptr<Box> boxFactory(char c, int w, int h) {}
